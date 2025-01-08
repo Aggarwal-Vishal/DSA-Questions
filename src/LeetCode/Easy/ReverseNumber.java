0294885f4994c4c4d63b06650e8689ebadee735c
Vishal Aggarwal <007vishal.aggarwal@gmail.com>
2025-01-08 12:58:19 +0530

    first commit

---
+++ src/LeetCode/Easy/ReverseNumber.java
@@ -0,0 +1,27 @@
+package LeetCode.Easy;
+
+import java.util.Scanner;
+
+public class ReverseNumber {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int n = in.nextInt();
+        int ans =0;
+
+            while (n > 0) {
+
+                int rem = n % 10;
+                ans = (ans * 10) + rem;
+                n = n / 10;
+
+
+
+            }
+        System.out.println(ans);
+
+
+        }
+    }
+
